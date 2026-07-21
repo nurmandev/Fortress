9f770f588e7403c03eee6f4e023cf75e9962c6cc
nurmandev <adebayour66265@gmail.com>
2026-07-21 15:19:02 +0100

    Configure next/image remotePatterns for Cloudinary hostname

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
   serverExternalPackages: ["cloudinary", "mongoose", "bcryptjs", "nodemailer"],
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+    ],
+  },
 };
 
 export default nextConfig;
